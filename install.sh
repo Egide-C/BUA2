7d0b1c289f0edca2e5d00aeb7bdfcfe6ce8d3691
Egide <46007968+Egide-C@users.noreply.github.com>
2025-01-11 05:36:56 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-curl -L bit.ly/BUAinstaller | bash
-curl -L https://github.com/DTJW92/game-downloader/raw/refs/heads/main/V3/install.sh  | bash
-curl -L bit.ly/foclabroc-switchoff | bash
+curl -L https://github.com/DTJW92/batocera-unofficial-addons/blob/main/gparted/gparted.sh | bash
+#bit.ly/BUAinstaller
+curl -L https://raw.githubusercontent.com/DTJW92/batocera-unofficial-addons/refs/heads/main/app/install.sh | bash
+curl -L https://github.com/DTJW92/game-downloader/raw/refs/heads/main/V3/install.sh | bash
+#bit.ly/foclabroc-switchoff
+curl -L https://raw.githubusercontent.com/foclabroc/batocera-switch/refs/heads/main/system/switch/extra/batocera-switch-installer.sh | bash
