6787356365fcafa01851135e3215faee84a17935
Egide <46007968+Egide-C@users.noreply.github.com>
2025-01-11 07:01:39 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -L https://github.com/DTJW92/batocera-unofficial-addons/blob/main/gparted/gparted.sh | bash
+curl -L https://github.com/DTJW92/batocera-unofficial-addons/raw/main/gparted/gparted.sh | bash
 #bit.ly/BUAinstaller
 curl -L https://raw.githubusercontent.com/DTJW92/batocera-unofficial-addons/refs/heads/main/app/install.sh | bash
 curl -L https://github.com/DTJW92/game-downloader/raw/refs/heads/main/V3/install.sh | bash
